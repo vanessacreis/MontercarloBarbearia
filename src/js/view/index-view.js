ddd09b831cd3354c592ee1474c222b9129550e9b
vanessacreis <vanessacreis@live.com>
2022-01-14 16:47:49 -0300

    Alterações no ícone dos itens do sobre no modo escuro

--- src/js/view/index-view.js
+++ src/js/view/index-view.js
@@ -1,8 +1,6 @@
 class View {
   trocaCorItens(tema) {
     console.log(tema);
-    $("#logo").attr("src", `./src/img/logo${tema}.svg`);
-    //   $(".logo").attr("src", `/src/img/icons/logo-${tema}.svg`);
-    //   $(".coracaoFilosofia").attr("src", `/src/img/coracao-${tema}.svg`);
+    $(".losango").attr("src", `./src/img/losango-${tema}.svg`);
   }
 }
