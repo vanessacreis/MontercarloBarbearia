1cb0ab75e289598a76514e6925335ae260ebd3a2
vanessacreis <vanessacreis@live.com>
2022-01-25 15:51:52 -0300

    Implementação do scroll suave para o sobre

--- src/js/index_main.js
+++ src/js/index_main.js
@@ -2,3 +2,16 @@ $(".trocaTema").click(() => {
   const controller = new Controller();
   controller.confereTema();
 });
+
+$('.menuLink[href^="#sobre"]').on("click", function (e) {
+  e.preventDefault();
+  const id = $(this).attr("href"),
+    targetOffset = $(id).offset().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: targetOffset,
+    },
+    450
+  );
+});
